80fabb06255beb0df2f9d8bbeabb57cc94fbf31c
Татьяна <korotyshtv33@gmail.com>
2023-07-13 17:58:51 +0300

    в тест добавлена проверка на корректность url, наличие форм логин и регистрация на странице логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
 
 
 
@@ -9,3 +11,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
+
+    page_login = LoginPage(browser, browser.current_url)
+    page_login.should_be_login_url()
+    page_login.should_be_login_form()
+    page_login.should_be_register_form()
